64eb6f139cb1c3cfc743088011074aef98a72bbb
dkadlecek <dkadlecek@grio.com>
2012-07-14 12:45:09 -0700

    Initial Commit

---
+++ spincard/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  spincard
+//
+//  Created by Douglas Kadlecek on 7/14/12.
+//  Copyright (c) 2012 Grio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ spincard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  spincard
+//
+//  Created by Douglas Kadlecek on 7/14/12.
+//  Copyright (c) 2012 Grio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
